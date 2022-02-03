4c069bdd13759483642c03c67c2c9691658b54ae
FiftyShadesofCode <philiprmcdavid@gmail.com>
2022-02-03 01:47:29 -0500

    doing a fresh upload in the morning when I have a fresh brain to fix something first

--- config/secrets.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    jwtSecret: process.env.JWT_SECRET || 'Keep it Secret, Keep it Safe!!',
-    environment: process.env.NODE_ENV || 'development',
-
-}
--- index.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config()
-
-const server = require('./api/server')
-
-const port = process.env.PORT || 8080
-server.listen(port, () => {
-  console.log(`\n*** Listening on port ${port} ***\n`)
-})
